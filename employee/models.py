da6a6c9cf0c8545f97a28dc39d3a0f217fa65f31
pradipmasal <pradipsmasal@gmail.com>
2024-11-20 20:21:18 +0530

    employee model created

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_id = models.EmailField(max_length=100,unique=True)
+    phone_number = models.CharField(max_length=12,blank=True)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    about_me = models.CharField(max_length=500)
+    
+
